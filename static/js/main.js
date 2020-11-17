1efe4be135ec121a803f964ab2f892b4bcc8d40e
Ben Somerville <70228881+bsmrvl@users.noreply.github.com>
2020-11-17 15:44:09 -0500

    js validation

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+var timer;
+
+function valquery(){
+    var goal = parseFloat(document.getElementById('queryform')['goal'].value);
+    if(goal < 0 || goal > 10000000){
+        flash('Goal must be between $0 and $10,000,000')
+        return false;
+    }
+}
+
+function flash(message){
+    var alertbox = document.getElementById('alertbox')
+    alertbox.innerHTML = message;
+    alertbox.style.visibility = 'visible';
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        alertbox.style.visibility = 'hidden';
+    }, 1500);
+
+}
