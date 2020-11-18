6af79e7b64c1442acffb312044c5c443f71380ec
Ben Somerville <70228881+bsmrvl@users.noreply.github.com>
2020-11-18 15:26:44 -0500

    pickle posixpath

--- model.py
+++ model.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 import pickle
-from pathlib import Path
+from pathlib import PosixPath
 
 def load_final_model():
     mod = None
-    with open(Path('tree.pickle'), 'rb') as handle:
+    with open(PosixPath('tree.pickle'), 'rb') as handle:
         mod = pickle.load(handle)
     return mod
 
 def nearest_neighbor_model():
     nn = None
-    with open(Path('nn.pickle'), 'rb') as handle:
+    with open(PosixPath('nn.pickle'), 'rb') as handle:
         nn = pickle.load(handle)
     return nn
 
@@ -22,7 +22,7 @@ def decision_tree_predict(blurb, goal, title, category):
 
 def get_nearest_neighbor(blurb):
     nlp = None
-    with open(Path('nlp.pickle'), 'rb') as handle:
+    with open(PosixPath('nlp.pickle'), 'rb') as handle:
         nlp = pickle.load(handle)
     blurb = nlp(blurb).vector
     # Returns an array of ID's of 10 nearest neighbors
