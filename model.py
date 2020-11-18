f579db26959fa7442996f7d2245e0c235dd89d07
Trevor Pedersen <trevr43@gmail.com>
2020-11-17 22:16:15 -0700

    Added decision tree model

--- model.py
+++ model.py
@@ -5,6 +5,16 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow import keras
 import pickle
 
+from sklearn.impute import SimpleImputer
+from category_encoders import OneHotEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeClassifier
+
+from category_encoders import OrdinalEncoder
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
+
 def build_model():
     pass
 
@@ -24,6 +34,17 @@ def load_model():
     new_model = tf.keras.models.load_model('./my_model/')
     return new_model
 
+def load_final_model():
+    mod = None
+    with open('tree.pickle', 'rb') as handle:
+        mod = pickle.load(handle)
+    return mod
+
+def decision_tree_predict(blurb, goal, category):
+    values = {'blurb':blurb, 'goal':goal, 'new_cat':category}
+    temp = pd.DataFrame(columns=['blurb', 'goal', 'new_cat']).append(values, ignore_index=True)
+    return decision_tree_model.predict([temp.iloc[0]])[0]
+
 def predict(description, category):
     description_val = [description]
     description_bow_val = tokenizer.texts_to_matrix(description_val)
@@ -36,12 +57,14 @@ def predict(description, category):
     test_embed_val = tokenizer.texts_to_sequences(description_val)
     max_seq_length = 170
     test_embed_val = keras.preprocessing.sequence.pad_sequences(test_embed_val, maxlen=max_seq_length)
-
     predictions = combined_model.predict([description_bow_val, variety_val] + [test_embed_val])
-
     return predictions.argmax(axis=-1)[0]
 
 tokenizer = load_tokenizer()
 combined_model = load_model()
 encoder = load_encoder()
-print(predict("hi", "Technology"))
+decision_tree_model = load_final_model()
+
+print(decision_tree_model)
+print(decision_tree_predict('abc', 10, "Games"))
+#print(predict("hi", "Technology"))
